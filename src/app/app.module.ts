4cb6a10efddf8664d6906531f2bb02651ec45c73
juiiii <jui.mundiwale@gmail.com>
2023-11-19 18:34:59 +0530

    first commit for portfolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LeftNavComponent } from './left-nav/left-nav.component';
+import { RightNavComponent } from './right-nav/right-nav.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavComponent,
+    LeftNavComponent,
+    RightNavComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
